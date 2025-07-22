7940696e7a9d0d879e5b07ce3bfa286092388df8
Dung Nguyen <nguyendung622@gmail.com>
2025-07-22 19:39:38 +0700

    Buổi 2. Khởi tạo DB

---
+++ QLSP/QLSP/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QLSP.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; }
+    }
+}
