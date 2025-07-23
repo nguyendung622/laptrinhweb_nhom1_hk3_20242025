002daae91acfa00d39b7cd3b2663d3abfd7aae9e
Dung Nguyen <nguyendung622@gmail.com>
2025-07-23 19:15:48 +0700

    Khởi tạo cho nhóm 2

---
+++ LTW2/HelloGroup2/Controllers/MathController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloGroup2.Controllers
+{
+    public class MathController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add(long a, long b)
+        {
+            long c = a + b;
+            MathResult mathResult = new MathResult
+            {
+                A = a,
+                B = b,
+                C = c,
+                Message = $"Tổng của {a} và {b} là {c}"
+            };
+            return View(mathResult);
+        }
+    }
+    public class MathResult
+    {
+        public long A { get; set; }
+        public long B { get; set; }
+        public long C { get; set; }
+        public string Message { get; set; }
+    }
+}
