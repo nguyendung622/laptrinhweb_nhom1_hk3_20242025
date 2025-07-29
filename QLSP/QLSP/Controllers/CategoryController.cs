3a9fc80a6ebf8161e88112fee135ddd0a45ef0d4
Dung Nguyen <nguyendung622@gmail.com>
2025-07-29 19:04:01 +0700

    Nhóm 1 - Buổi 5

--- QLSP/QLSP/Controllers/CategoryController.cs
+++ QLSP/QLSP/Controllers/CategoryController.cs
@@ -11,22 +11,33 @@ namespace QLSP.Controllers
         {
             _context = context;
         }
-        public IActionResult Index(string keyWord="")
+        public IActionResult Index(string keyWord = "", int pageIndex = 1, int pageSize = 2)
         {
+            var pagingInfo = new PagingInfo
+            {
+                PageSize = pageSize,
+                PageIndex = pageIndex,
+            };
             CategoryViewModel model = new CategoryViewModel();
             model.KeyWord = keyWord;
             if (string.IsNullOrEmpty(keyWord))
             {
-                model.Categories = _context.Categories.
-                    Select(e => new CategoryDTO { Id = e.Id, Name = e.Name }).ToList();
+                var ls = _context.Categories.
+                    Select(e => new CategoryDTO { Id = e.Id, Name = e.Name });
+
+                pagingInfo.Total = ls.Count();
+                model.Categories = ls.Skip(pagingInfo.PageSize * (pagingInfo.PageIndex - 1))
+                                     .Take(pagingInfo.PageSize)
+                                     .ToList();
             }
             else
             {
                 var ls = _context.Categories.Where(e => e.Name.Contains(keyWord)).
-                    Select(e => new CategoryDTO { Id = e.Id, Name = e.Name }).ToList();
-
-                model.Categories = ls;
+                    Select(e => new CategoryDTO { Id = e.Id, Name = e.Name });
+                pagingInfo.Total = ls.Count();
+                model.Categories = ls.ToList();
             }
+            model.PagingInfo = pagingInfo;
             return View(model);
         }
 
@@ -38,10 +49,10 @@ namespace QLSP.Controllers
         [HttpPost]
         public IActionResult Create(CategoryViewModel model)
         {
-            var category = _context.Categories.Where(e=>e.Name == model.Request.Name).FirstOrDefault();
+            var category = _context.Categories.Where(e => e.Name == model.Request.Name).FirstOrDefault();
             if (category != null)
             {
-                ViewData["name"] = model.Request.Name;   
+                ViewData["name"] = model.Request.Name;
                 ViewData["message"] = "Tên danh mục này đã tồn tại";
                 return View();
             }
@@ -85,6 +96,29 @@ namespace QLSP.Controllers
                 return View(model);
             }
         }
+        [HttpGet]
+        public IActionResult Delete(long id)
+        {
+            var category = _context.Categories.Where(e => e.Id == id).Select(e => new CategoryDTO
+            {
+                Id = e.Id,
+                Name = e.Name
+            }).FirstOrDefault();
+            var model = new CategoryViewModel();
+            model.Response = category;
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Delete(CategoryViewModel model)
+        {
+            var category = _context.Categories.Where(e => e.Id == model.Request.Id).FirstOrDefault();
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+        }
 
         List<CategoryDTO> genData()
         {
@@ -101,10 +135,26 @@ namespace QLSP.Controllers
         public List<CategoryDTO> Categories { get; set; }
         public CategoryDTO Request { get; set; }
         public CategoryDTO Response { get; set; }
+        public PagingInfo PagingInfo { get; set; }
     }
     public class CategoryDTO
     {
         public int Id { get; set; }
         public string Name { get; set; }
+    }
+
+    public class PagingInfo
+    {
+        public int PageIndex { get; set; } = 1;
+        public int PageSize { get; set; } = 5;
+        public int Total { get; set; }
+        public int PageCount
+        {
+            get
+            {
+                return Total / PageSize + (Total % PageSize != 0 ? 1 : 0);
+            }
+        }
+
     }
 }
